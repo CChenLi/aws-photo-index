3b5da68884130c6c9ff0265ce2021fc780779325
CChenLi <lichen918@g.ucla.edu>
2022-03-22 03:21:27 -0400

    Add LF1

--- LF1/lambda_function.py
+++ LF1/lambda_function.py
@@ -17,7 +17,7 @@ region = 'us-east-1' # For example, us-west-1
 service = 'es'
 credentials = boto3.Session().get_credentials()
 awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
-host = "search-photo-bs2ezi4qxnq24fuvttqxhu5mma.us-east-1.es.amazonaws.com"
+host = "search-photo-search-hyksul2bdeyoenyvplrowctj74.us-east-1.es.amazonaws.com"
 es_client = OpenSearch(
     hosts = [{'host': host, 'port': 443}],
     http_auth = awsauth,
@@ -25,7 +25,6 @@ es_client = OpenSearch(
     verify_certs = True,
     connection_class = RequestsHttpConnection
 )
-
 
 # --------------- Helper Functions to call Rekognition APIs ------------------
 
@@ -37,21 +36,8 @@ def detect_faces(bucket, key):
 
 def detect_labels(bucket, key):
     response = rekognition.detect_labels(Image={"S3Object": {"Bucket": bucket, "Name": key}})
-
-    # Sample code to write response to DynamoDB table 'MyTable' with 'PK' as Primary Key.
-    # Note: role used for executing this Lambda function should have write access to the table.
-    #table = boto3.resource('dynamodb').Table('MyTable')
-    #labels = [{'Confidence': Decimal(str(label_prediction['Confidence'])), 'Name': label_prediction['Name']} for label_prediction in response['Labels']]
-    #table.put_item(Item={'PK': key, 'Labels': labels})
     labels = [label_prediction['Name'] for label_prediction in response['Labels']]
     return labels
-
-
-def index_faces(bucket, key):
-    # Note: Collection has to be created upfront. Use CreateCollection API to create a collecion.
-    #rekognition.create_collection(CollectionId='BLUEPRINT_COLLECTION')
-    response = rekognition.index_faces(Image={"S3Object": {"Bucket": bucket, "Name": key}}, CollectionId="BLUEPRINT_COLLECTION")
-    return response
 
 def put_index(item):
     es_client.index(index="photo", doc_type="_doc", id=item["objectKey"], body=item)
@@ -108,8 +94,8 @@ def lambda_handler(event, context):
 
             put_index(item)
             # print(es_client.get(index="photo", doc_type="_doc", id=key))
-            res = es_search("Dog")
-            print("search 'Dog': ", res)
+            # res = es_search("Dog")
+            # print("search 'Dog': ", res)
         else:
             print("Non-photo upload: ", key)
 
